b627dd4a369610414bae6ac6e42ee19ac1f30b05
NgoNguyenThuHuong <ngo24616@gmail.com>
2026-03-25 20:10:27 +0700

    Delete webbanhang directory

--- webbanhang/check_users.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'app/config/database.php';
-$db = (new Database())->getConnection();
-$stmt = $db->query("SELECT id, username, password, fullname, role FROM users");
-$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($users, JSON_PRETTY_PRINT);
--- webbanhang/verify_pw.php
+++
@@ -1,7 +0,0 @@
-<?php
-$hash = '$2y$10$vIz6SccCISPUgl4r3qR2K.ZYzBgSSyc/LCAAhig/PG7/TN0Ts8UJG';
-if (password_verify('123', $hash)) {
-    echo "Password 123 is valid\n";
-} else {
-    echo "Password 123 is invalid\n";
-}
